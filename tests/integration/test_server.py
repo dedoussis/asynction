cca38e8bf3dc7f37d93ae82d2279f4ada4509917
Dimitrios Dedoussis <ddedoussis@gmail.com>
2021-05-07 09:09:09 +0100

    Exception API

--- tests/integration/test_server.py
+++ tests/integration/test_server.py
@@ -1,11 +1,12 @@
 from typing import Callable
 
-import jsonschema
 import pytest
 from faker import Faker
 from flask.app import Flask
 from flask_socketio import SocketIO
 
+from asynction.exceptions import BindingsValidationException
+from asynction.exceptions import PayloadValidationException
 from tests.fixtures import FixturePaths
 
 AsynctionFactory = Callable[..., SocketIO]
@@ -35,12 +36,16 @@ def test_client_emits_and_receives_message_successfully(
         flask_app, flask_test_client=flask_test_client
     )
     socketio_test_client.get_received()
+
     message_to_echo = faker.pystr()
     socketio_test_client.emit("echo", message_to_echo)
+
     received = socketio_test_client.get_received()
+
     assert len(received) == 1
-    assert len(received[0]["args"]) == 1
-    assert received[0]["args"][0] == message_to_echo
+    received_args = received[0]["args"]
+    assert len(received_args) == 1
+    assert received_args[0] == message_to_echo
 
 
 def test_client_emitting_invalid_message(
@@ -55,7 +60,7 @@ def test_client_emitting_invalid_message(
         flask_app, flask_test_client=flask_test_client
     )
     socketio_test_client.get_received()
-    with pytest.raises(jsonschema.ValidationError):
+    with pytest.raises(PayloadValidationException):
         socketio_test_client.emit("echo", faker.pyint())
 
 
@@ -65,7 +70,7 @@ def test_server_emitting_invalid_message(
     faker: Faker,
 ):
     socketio_server = asynction_socketio_server_factory(spec_path=fixture_paths.echo)
-    with pytest.raises(jsonschema.ValidationError):
+    with pytest.raises(PayloadValidationException):
         socketio_server.emit("echo", faker.pyint())
 
 
@@ -97,7 +102,7 @@ def test_client_connecting_with_invalid_bindings(
     socketio_server = asynction_socketio_server_factory(spec_path=fixture_paths.echo)
     flask_test_client = flask_app.test_client()
 
-    with pytest.raises(jsonschema.ValidationError):
+    with pytest.raises(BindingsValidationException):
         socketio_server.test_client(
             flask_app,
             namespace="/admin",
@@ -122,3 +127,34 @@ def test_client_can_connect_to_server_that_uses_server_name(
     )
     socketio_test_client.get_received()
     assert True
+
+
+def test_client_emits_invalid_msg_and_server_emits_back_via_validation_error_handler(
+    asynction_socketio_server_factory: AsynctionFactory,
+    flask_app: Flask,
+    fixture_paths: FixturePaths,
+    faker: Faker,
+):
+    socketio_server = asynction_socketio_server_factory(
+        spec_path=fixture_paths.echo,
+    )
+
+    flask_test_client = flask_app.test_client()
+
+    namespace_with_error_feedback = "/echo_with_error_feedback"
+
+    socketio_test_client = socketio_server.test_client(
+        flask_app,
+        namespace=namespace_with_error_feedback,
+        flask_test_client=flask_test_client,
+    )
+
+    socketio_test_client.get_received(namespace_with_error_feedback)
+
+    message_to_echo = faker.pyint()
+    socketio_test_client.emit(
+        "echo", message_to_echo, namespace=namespace_with_error_feedback
+    )
+    received = socketio_test_client.get_received(namespace_with_error_feedback)
+    assert len(received) == 1
+    assert received[0]["name"] == "echo errors"

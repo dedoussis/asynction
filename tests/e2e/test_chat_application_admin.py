3e15763b7cbb4b75960ac70b7d954762ec1298d8
Dimitrios Dedoussis <ddedoussis@gmail.com>
2021-08-11 13:08:56 +0100

    Minor enhancements in the e2e testing suite (#61)
    * Minor enhancements in e2e testing suite
    
    * Properly rendered markdown task lists

--- tests/e2e/test_chat_application_admin.py
+++ tests/e2e/test_chat_application_admin.py
@@ -1,4 +1,5 @@
 from typing import Callable
+from unittest.mock import Mock
 
 import pytest
 import socketio
@@ -9,9 +10,20 @@ def test_admin_namespace_with_correct_token(
     server_url: str, socketio_client_factory: Callable[[], socketio.Client]
 ):
     admin_client = socketio_client_factory()
+
+    server_metric_event = "server metric"
+
+    mock_ack = Mock()
+
+    @admin_client.on(server_metric_event, namespace="/admin")
+    def _server_metric(data):
+        assert data["name"] == "CPU_COUNT"
+        assert data["value"] > 0
+        mock_ack(server_metric_event)
+
     admin_client.connect(f"{server_url}?token=admin", namespaces=["/admin"], wait=True)
-    assert True
     admin_client.disconnect()
+    mock_ack.assert_called_once_with(server_metric_event)
 
 
 def test_admin_namespace_with_faulty_token(

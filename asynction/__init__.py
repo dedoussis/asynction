f9882c3bdc4470bf38ae9ad36cf3e992d298b3bd
Dimitrios Dedoussis <dimitrios@dedouss.is>
2021-08-30 10:33:35 +0100

    Dunder all for root dunder init

--- asynction/__init__.py
+++ asynction/__init__.py
@@ -1,9 +1,21 @@
 # flake8: noqa: F401
+
+__all__ = [
+    "AsynctionSocketIO",
+    "AsynctionException",
+    "ValidationException",
+    "PayloadValidationException",
+    "BindingsValidationException",
+    "MessageAckValidationException",
+]
+
 from asynction.exceptions import *
 from asynction.server import AsynctionSocketIO
 
 try:
     from asynction.mock_server import MockAsynctionSocketIO
+
+    __all__ = [*__all__, "MockAsynctionSocketIO"]
 except ImportError:
     # Mock support may not be available if the mock
     # extra requirements have not been installed
